dd943be9bafa17c1f285d00005ef1e0c3b2b5263
aguilaraul <raula@live.com>
2019-01-06 19:52:35 -0800

    Finished Caesar Cipher

--- Assignments/Assignment-4/CaesarCipher.java
+++ Assignments/Assignment-4/CaesarCipher.java
@@ -4,54 +4,160 @@
 import java.util.Scanner;
 
 public class CaesarCipher {
+	public static String encrypt(String s, int shift) {
+		if (shift>26) {
+			shift = shift%26;
+		}
+		String cipher = "";
+	      	int length = s.length();
+  		for(int i = 0; i<length; i++) {
+    			char l = s.charAt(i);
+    			if (Character.isLetter(l)) {
+      				if (Character.isLowerCase(l)){
+					char c = (char) (l+shift);
+					if (c>'z') {
+						cipher += (char) (l - (26-shift));
+					} else if (c<'a') {
+						cipher += (char) (l + (26+shift));
+					} else {
+						cipher += c;
+					}
+				} else if(Character.isUpperCase(l)) {
+					char c = (char) (l+shift);
+					if (c>'Z') {
+						cipher += (char) (l - (26-shift));
+					} else if (c<'A') {
+				  		cipher += (char) (l + (26+shift));
+					} else {
+				    		cipher += c;
+					}
+				}
+			} else {
+				cipher += l;
+			}
+		}
+		return cipher;
+	}
+	public static String decrypt(String s, int shift) {
+		if (shift>26) {
+			shift = shift%26;
+		}
 
-public static String encrypt(String s, int shift) {
-  if (shift>26) shift = shift%26;
+	      	String cipher = "";
+	      	int length = s.length();
+	      	for(int i = 0; i<length; i++) {
+		    	char l = s.charAt(i);
+		    	if (Character.isLetter(l)) {
+			  	if (Character.isLowerCase(l)){
+					char c = (char) (l-shift);
+					if (c>'z') {
+				      		cipher += (char) (l - (26-shift));
+					} else if (c<'a') {
+				    		cipher += (char) (l + (26-shift));
+					} else {
+				      		cipher += c;
+					}
+			  	} else if(Character.isUpperCase(l)) {
+			      		char c = (char) (l-shift);
+			      		if (c>'Z') {
+				    		cipher += (char) (l - (26-shift));
+			      		} else if (c<'A') {
+				  		cipher += (char) (l + (26-shift));
+			      		} else {
+				    		cipher += c;
+			      		}
+			  	}
+		    	} else {
+			  	cipher += l;
+		    	}
+	      	}
+	      	return cipher;
+	}
 
-  String cipher = "";
-  int length = s.length();
-  for(int i = 0; i<length; i++) {
-    char l = s.charAt(i);
-    if (Character.isLetter(l)) {
-      if (Character.isLowerCase(l)){
-        char c = (char) (l+shift);
-        if (c>'z') {
-          cipher += (char) (l - (26-shift));
-        } else if (c<'a') {
-            cipher += (char) (l + (26+shift));
-        } else {
-          cipher += c;
-        }
-      } else if(Character.isUpperCase(l)) {
+	public static String augustusCipher(String s) {
+	      	String cipher = "";
+		int length = s.length();
 
-          char c = (char) (l+shift);
-          if (c>'Z') {
-            cipher += (char) (l - (26-shift));
-          } else if (c<'A') {
-              cipher += (char) (l + (26+shift));
-          } else {
-            cipher += c;
-          }
-      }
-    } else {
-      cipher += l;
-    }
-  }
-  return cipher;
+		for(int i = 0; i<length; i++) {
+		    	char l = s.charAt(i);
+		    	if (Character.isLetter(l)) {
+			  		if (Character.isLowerCase(l)){
+						char c = (char) (l+1);
+						if (c>'z') {
+				      		cipher += "aa";
+						} else {
+				      		cipher += c;
+						}
+			  		} else if(Character.isUpperCase(l)) {
+			      		char c = (char) (l+1);
+			      		if (c>'Z') {
+				    		cipher += "AA";
+			      		} else {
+				    		cipher += c;
+			      		}
+			  		}
+		    	} else {
+			  		cipher += l;
+			  	}
+		}
+		return cipher;
+	}
 
-}
+	public static String augustusDecipher(String s) {
+		String cipher = "";
+		int length = s.length();
 
-    public static void main(String[] args) {
-      String message;
-      int shift = 0;
-      System.out.printf("%s%n", "Please enter a message you'd like to encrypt");
-      Scanner scan = new Scanner(System.in);
-      message = scan.nextLine();
-      System.out.printf("%s%n", "Please enter the shift you'd like use");
-      shift = scan.nextInt();
-      System.out.println(message);
-      String cipher = encrypt(message, shift);
-      System.out.println(cipher);
+		for(int i = 0; i<length; i++) {
+		    	char l = s.charAt(i);
+		    	if (Character.isLetter(l)) {
+			  		if (Character.isLowerCase(l)){
+						char c = (char) (l-1);
+						if (c<'a') {
+				      		cipher += "z";
+				      		i++;
+						} else {
+				      		cipher += c;
+						}
+			  		} else if(Character.isUpperCase(l)) {
+			      		char c = (char) (l-1);
+			      		if (c<'A') {
+				    		cipher += "Z";
+				    		i++;
+			      		} else {
+				    		cipher += c;
+			      		}
+			  		}
+		    	} else {
+			  		cipher += l;
+			  	}
+		}
+		return cipher;
+	}	
+	
+	public static void main(String[] args) {
+	  	Scanner scan = new Scanner(System.in);
+	  	String message;
+		int shift = 0;
+	  	
+		System.out.printf("%s%n", "Please enter a message you'd like to encrypt");
+	  	message = scan.nextLine();
+		System.out.printf("%s%n", "Please enter the shift you'd like use");
+	  	shift = scan.nextInt();
+
+	  	System.out.println();
+	  	System.out.println("Caesar Cipher:");
+	  	System.out.println(message);
+	  	String cipher = encrypt(message, shift);
+	  	System.out.println(cipher);
+	  	System.out.println(decrypt(cipher, shift));
+	  	
+		System.out.println();
+		String encryptedAugustus = augustusCipher(message);
+		String decryptedAugustus = augustusDecipher(encryptedAugustus);
+	  	System.out.println("Augustus Cipher:");
+	  	System.out.println(message);
+		System.out.println(encryptedAugustus);
+		System.out.println(decryptedAugustus);
     }
 
 }
